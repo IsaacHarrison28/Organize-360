7ceb6da6d7201286247542abe1d15c781bea87e1
Isaac Harrison <sateonlineservices@gmail.com>
2024-02-24 00:20:10 +0200

    adjusted todo frontend designs

--- frontend/src/components/Todos/SingleTodoCardDesign.tsx
+++ frontend/src/components/Todos/SingleTodoCardDesign.tsx
@@ -1,5 +1,5 @@
 import { Box, FormControl, FormLabel, Switch, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function SingleTodoCard() {
   const [isCompletedState, changeIsCompletedState] = useState(false);
@@ -14,68 +14,87 @@ export default function SingleTodoCard() {
       }}
       mx={{ base: "-1" }}
     >
-      <Box
-        flex="1 0 calc(50% - 0.5rem)"
-        m={{ base: "1" }}
-        sx={{
-          borderRadius: "12",
-          background: "#F4F2FF",
-        }}
-        p={4}
-      >
-        <Box>
-          <Text
-            fontSize={{ base: "md", lg: "20" }}
-            sx={{
-              fontWeight: "bold",
-              fontFamily: "DM Sans",
-              textAlign: "left",
-              paddingInline: "1",
-            }}
-            marginBottom={{ base: "0.5rem", lg: "1rem" }}
-            lineHeight="1"
-            noOfLines={[1, 2]}
-          >
-            Make some books clean because mom needs them clean and I don't want
-            to be the bad kid
-          </Text>
-        </Box>
-        <Box>
-          <Text
-            fontSize={{ base: "sm", lg: "0.99rem" }}
-            sx={{
-              fontFamily: "DM Sans",
-              textAlign: "left",
-            }}
-            lineHeight="1.2"
-            noOfLines={[2, 3]}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet hic
-            eos libero suscipit dolorem incidunt itaque similique nostrum porro
-            fugiat aspernatur minus quibusdam, nulla quia ipsum nemo
-            voluptatibus labore error.
-          </Text>
-        </Box>
-        <Box my="4">
-          <FormControl
-            display="flex"
-            alignItems="center"
-            justifyContent="flex-end"
-          >
-            <FormLabel htmlFor="isCompleted" mb="0">
-              <Text fontSize={{ base: "md", lg: "1rem" }}>Completed?</Text>
-            </FormLabel>
-            <Switch
-              id="isCompleted"
-              isChecked={isCompletedState}
-              onChange={() => {
-                changeIsCompletedState(!isCompletedState);
-              }}
-              colorScheme="green"
-            />
-          </FormControl>
-        </Box>
-      </Box>
+      <TodoCardTemplate
+        isCompletedState={isCompletedState}
+        changeIsCompletedState={changeIsCompletedState}
+      />
     </Box>
   );
 }
+
+//define the props types for the TodoCardTemplate component for better typescript functionalitu and type annotation
+interface TodoCardTemplatePropTypes {
+  changeIsCompletedState: React.Dispatch<React.SetStateAction<boolean>>;
+  isCompletedState: boolean;
+}
+
+const TodoCardTemplate: React.FC<TodoCardTemplatePropTypes> = ({
+  isCompletedState,
+  changeIsCompletedState,
+}) => {
+  return (
+    <Box
+      flex="1 0 calc(50% - 0.5rem)"
+      m={{ base: "1" }}
+      sx={{
+        borderRadius: "12",
+        background: "#F4F2FF",
+      }}
+      p={4}
+    >
+      <Box>
+        <Text
+          fontSize={{ base: "md", lg: "20" }}
+          sx={{
+            fontWeight: "bold",
+            fontFamily: "DM Sans",
+            textAlign: "left",
+            paddingInline: "1",
+          }}
+          marginBottom={{ base: "0.5rem", lg: "1rem" }}
+          lineHeight="1"
+          noOfLines={[1, 2]}
+        >
+          Make some books clean because mom needs them clean and I don't want to
+          be the bad kid
+        </Text>
+      </Box>
+      <Box>
+        <Text
+          fontSize={{ base: "sm", lg: "0.99rem" }}
+          sx={{
+            fontFamily: "DM Sans",
+            textAlign: "left",
+          }}
+          lineHeight="1.2"
+          noOfLines={[2, 3]}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet hic
+          eos libero suscipit dolorem incidunt itaque similique nostrum porro
+          fugiat aspernatur minus quibusdam, nulla quia ipsum nemo voluptatibus
+          labore error.
+        </Text>
+      </Box>
+      <Box my="4">
+        <FormControl
+          display="flex"
+          alignItems="center"
+          justifyContent="flex-end"
+        >
+          <FormLabel htmlFor="isCompleted" mb="0">
+            <Text fontSize={{ base: "md", lg: "1rem" }}>Completed?</Text>
+          </FormLabel>
+          <Switch
+            id="isCompleted"
+            isChecked={isCompletedState}
+            onChange={() => {
+              changeIsCompletedState(!isCompletedState);
+            }}
+            colorScheme="green"
+            size="md"
+          />
+        </FormControl>
+      </Box>
+    </Box>
+  );
+};
--- frontend/src/components/Todos/TodoDisplayHeading.tsx
+++ frontend/src/components/Todos/TodoDisplayHeading.tsx
@@ -1,5 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import { Clipboard } from "../../svg-icon-components/svgs";
+import { AddIcon } from "@chakra-ui/icons";
 
 export default function TodoHeadDisplay() {
   return (
@@ -7,24 +8,47 @@ export default function TodoHeadDisplay() {
       sx={{
         display: "flex",
         flexAlign: "center",
-        columnGap: "1rem",
+        justifyContent: "space-between",
       }}
       my={{ base: "5", lg: "10" }}
+      px={{ base: "8", lg: "16" }}
     >
-      <Box>
-        <Clipboard />
-      </Box>
-      <Text
+      <Box
         sx={{
-          alignSelf: "flex-end",
-          fontWeight: "bold",
-          color: "#2B1887",
-          fontFamily: "DM Sans",
+          display: "flex",
+          alignItems: "center",
+          columnGap: "1rem",
         }}
-        fontSize={{ base: "lg", lg: "1.5rem" }}
       >
-        To-Do
-      </Text>
+        <Box>
+          <Clipboard />
+        </Box>
+        <Text
+          sx={{
+            alignSelf: "flex-end",
+            fontWeight: "bold",
+            color: "#2B1887",
+            fontFamily: "DM Sans",
+          }}
+          fontSize={{ base: "lg", lg: "1.5rem" }}
+        >
+          To-Do
+        </Text>
+      </Box>
+      <Box
+        sx={{
+          background: "#F4F2FF",
+          borderRadius: "50%",
+        }}
+        px={{ base: "3.5", lg: "2" }}
+        py={{ base: "3", lg: "2" }}
+      >
+        <AddIcon
+          color="black"
+          fontSize={{ base: "larger", lg: "x-large" }}
+          sx={{ borderRadius: "50%" }}
+        />
+      </Box>
     </Box>
   );
 }
