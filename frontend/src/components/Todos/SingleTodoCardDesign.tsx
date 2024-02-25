b8b7198a2e92e582de4ea57991dfa94a35a1df89
Isaac Harrison <sateonlineservices@gmail.com>
2024-02-25 02:27:32 +0200

    integrated data fecthing from backend for todos

--- frontend/src/components/Todos/SingleTodoCardDesign.tsx
+++ frontend/src/components/Todos/SingleTodoCardDesign.tsx
@@ -1,10 +1,76 @@
-import { Box, FormControl, FormLabel, Switch, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Switch,
+  Text,
+  Icon,
+} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import * as ReactDOM from "react-dom";
+import { FaClipboardList } from "react-icons/fa";
 
 export default function SingleTodoCard() {
-  const [isCompletedState, changeIsCompletedState] = useState(false);
+  const [isCompletedState, changeIsCompletedState] = useState<boolean>(false);
+  const [listOfTodos, changeListOfTodos] = useState<TodosObjectTemplate[]>([]);
+
+  //interface for the ListOfTodos array for type annotation
+  interface TodosObjectTemplate {
+    id: number;
+    title: string;
+    description: string;
+    status: string;
+    createdOn: Date;
+  }
+
+  //fetch todos funtion
+  function fetchTodos() {
+    fetch("http://localhost:8080/todos", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        changeListOfTodos(data);
+      });
+  }
+
+  //fetch all todos from database through the api
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  if (listOfTodos.length > 1) {
+    const EmptyTodos = () => {
+      return (
+        <Box my={8} background="#F4F2FF" p={24} borderRadius="12">
+          <Text
+            sx={{
+              fontFamily: "DM Sans",
+              color: "#2B1887",
+              fontWeight: "bold",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              rowGap: "1rem",
+            }}
+            fontSize="lg"
+          >
+            <Icon as={FaClipboardList} boxSize={50} color="#2B1887" />
+            You do not have any todos.
+          </Text>
+        </Box>
+      );
+    };
+
+    ReactDOM.render(<EmptyTodos />, document.getElementById("todos_container"));
+  }
+
+  console.log(listOfTodos);
+
   return (
     <Box
+      id="todos_container"
       sx={{
         display: "flex",
         alignItems: "center",
@@ -13,24 +79,23 @@ export default function SingleTodoCard() {
         flexWrap: "wrap",
       }}
       mx={{ base: "-1" }}
-    >
-      <TodoCardTemplate
-        isCompletedState={isCompletedState}
-        changeIsCompletedState={changeIsCompletedState}
-      />
-    </Box>
+    ></Box>
   );
 }
 
 //define the props types for the TodoCardTemplate component for better typescript functionalitu and type annotation
 interface TodoCardTemplatePropTypes {
-  changeIsCompletedState: React.Dispatch<React.SetStateAction<boolean>>;
+  // changeIsCompletedState: React.Dispatch<React.SetStateAction<boolean>>;
   isCompletedState: boolean;
+  description: string;
+  title: string;
 }
 
 const TodoCardTemplate: React.FC<TodoCardTemplatePropTypes> = ({
   isCompletedState,
-  changeIsCompletedState,
+  // changeIsCompletedState,
+  title,
+  description,
 }) => {
   return (
     <Box
@@ -55,8 +120,7 @@ const TodoCardTemplate: React.FC<TodoCardTemplatePropTypes> = ({
           lineHeight="1"
           noOfLines={[1, 2]}
         >
-          Make some books clean because mom needs them clean and I don't want to
-          be the bad kid
+          {title}
         </Text>
       </Box>
       <Box>
@@ -69,10 +133,7 @@ const TodoCardTemplate: React.FC<TodoCardTemplatePropTypes> = ({
           lineHeight="1.2"
           noOfLines={[2, 3]}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet hic
-          eos libero suscipit dolorem incidunt itaque similique nostrum porro
-          fugiat aspernatur minus quibusdam, nulla quia ipsum nemo voluptatibus
-          labore error.
+          {description}
         </Text>
       </Box>
       <Box my="4">
@@ -88,7 +149,7 @@ const TodoCardTemplate: React.FC<TodoCardTemplatePropTypes> = ({
             id="isCompleted"
             isChecked={isCompletedState}
             onChange={() => {
-              changeIsCompletedState(!isCompletedState);
+              // changeIsCompletedState(!isCompletedState);
             }}
             colorScheme="green"
             size="md"
